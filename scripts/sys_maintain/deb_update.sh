b75ed6399e69b4d0e48d26b0b124e7912247475e
Artur Augustyniak <artur@aaugustyniak.pl>
2025-06-30 18:54:43 +0200

    ignore deferred

--- scripts/sys_maintain/deb_update.sh
+++ scripts/sys_maintain/deb_update.sh
@@ -3,7 +3,8 @@
 # export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -y update
 sudo aptitude -y safe-upgrade
-sudo apt-get --with-new-pkgs -y upgrade -o APT::Get::Always-Include-Phased-Updates=true
+sudo apt-get --with-new-pkgs -y upgrade 
+#-o APT::Get::Always-Include-Phased-Updates=true
 sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
 sudo apt-get -y autoclean
